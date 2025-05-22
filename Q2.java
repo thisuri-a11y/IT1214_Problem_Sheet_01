f235f327e26b9e57c85ad9f15bb1f85d60ec86a5
thisuri-a11y <mandirathisuri@gmail.com>
2025-05-22 00:44:03 +0000

    created

--- Q2.java
+++ Q2.java
@@ -36,3 +36,20 @@ public class Vehicle {
   }
 }
 
+public class ParkingLot {
+
+}
+
+public class Main {
+    public static void main(String[] args) {
+        ParkingLot lot = new ParkingLot();
+
+        lot.parkVehicle(new Vehicle("ABC123", "John Doe", 2));
+        lot.parkVehicle(new Vehicle("XYZ789", "Jane Smith", 4));
+        lot.parkVehicle(new Vehicle("LMN456", "Bob Brown", 1));
+
+    
+    }
+}
+
+
