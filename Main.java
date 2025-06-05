99b0c6540000ea8c610adbc6a682d44084758765
thisuri-a11y <mandirathisuri@gmail.com>
2025-06-05 07:06:26 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,51 +1,51 @@
-class Student{
+import java.util.ArrayList;
+
+class Student {
   private int studentId;
   private String name;
   private int daysAttended;
 
-//constructor
-
-  public Student(int studentId,String name,int daysAttended){
-    this.studentId=studentId;
-    this.name=name;
-    this.daysAttended;
+  // Constructor
+  public Student(int studentId, String name, int daysAttended) {
+    this.studentId = studentId;
+    this.name = name;
+    this.daysAttended = daysAttended;
   }
 
-  //getters
-
-  public int getStudentId(){
+  // Getters
+  public int getStudentId() {
     return studentId;
   }
 
-  public String getName(){
+  public String getName() {
     return name;
   }
 
-  public int getDaysAttended(){
+  public int getDaysAttended() {
     return daysAttended;
   }
 
-//setters
-
-  public void setStudentId(int studentId){
-    this.studentId=studentId;
+  // Setters
+  public void setStudentId(int studentId) {
+    this.studentId = studentId;
   }
 
- public void setName(String name){
-    this.name=name;
+  public void setName(String name) {
+    this.name = name;
   }
 
-   public void setDaysAttended(int daysAttended){
-    this.daysAttended=daysAttended;
+  public void setDaysAttended(int daysAttended) {
+    this.daysAttended = daysAttended;
   }
 
-//method
-public void display(){
-  System.out.println("ID: "+ studentId + ",Name: "+ name + ",DaysAttended: "+ daysAttended);
-  } 
+  // Method
+  public void display() {
+    System.out.println("ID: " + studentId + ", Name: " + name + ", Days Attended: " + daysAttended);
+  }
 }
 
-class Classroom{  private ArrayList<Student> students;
+class Classroom {
+  private ArrayList<Student> students;
 
   public Classroom() {
     students = new ArrayList<>();
@@ -59,18 +59,20 @@ class Classroom{  private ArrayList<Student> students;
     for (Student student : students) {
       student.display();
     }
-  
+  }
 }
 
-public class Main{
-  public static void main(String[] args){}
-  //create classroom
-  Classroom classroom = new Classroom();
+public class Main {
+  public static void main(String[] args) {
 
-classroom.addStudent(newStudent(101,"Thisu",11));
-classroom.addStudent(newStudent(102,"Nike",12));
-classroom.addStudent(newStudent(103,"Kula",14));
+    Classroom classroom = new Classroom();
+
   
+    classroom.addStudent(new Student(101, "Thisu", 11));
+    classroom.addStudent(new Student(102, "Nike", 12));
+    classroom.addStudent(new Student(103, "Kula", 14));
+
+  
+    classroom.displayAllStudents();
+  }
 }
-
-
